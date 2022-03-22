35b864752c5df00535d46f1a38ccf0652c69c2c2
Lauren <lauren@laurens-mbp.home>
2022-03-22 07:56:00 +0000

    Use stylesheet

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,31 +1,16 @@
 import * as React from "react"
+import "../index.css"
 
 import Sidebar from "../components/Sidebar"
 
-const columns = {
-  display: "flex",
-  flexDirection: "row",
-  flexWrap: "wrap",
-  width: "100vw",
-  height: "100vh",
-}
-
-const sidebar = {
-  flex: "20%",
-  backgroundColor: "lightskyblue",
-}
-
-const content = {
-  flex: "80%",
-}
 
 const App = () => {
   return (
-    <div style={columns}>
-     <div style={sidebar}>
+    <div class="columns">
+     <div class="sidebar">
        <Sidebar />
      </div>
-     <div style={content}>
+     <div class="content">
        Column 2
     </div>
   </div>
