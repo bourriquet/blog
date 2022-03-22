914bd072bd62270964e1cf8d90264d1cb9e892b5
Lauren <lauren@laurens-mbp.home>
2022-03-22 08:37:14 +0000

    Add sidebar image

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,13 @@
 import * as React from "react"
+import "../index.css"
+import image from "../images/sidebar_image.jpeg"
 
 const Sidebar = () => {
   return (
-    <div>
-      Sidebar
+    <div class="sidebar-container">
+      <div class="sidebar-image">
+        <img width="90%" src={image} />
+      </div>
     </div>
   )
 }
