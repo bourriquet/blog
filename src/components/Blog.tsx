5bf3b2a60cf0a1e17ad9b110b2c501399e5562b2
Lauren <lauren@laurens-mbp.home>
2022-03-23 21:49:28 +0000

    Blog post layout

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -30,6 +30,7 @@ const Blog = () => {
           page.date = result.properties.Date.date.start
           page.location = result.properties.Location.rich_text[0].plain_text
           page.caption = result.properties.Caption.rich_text[0].plain_text
+          page.photo = result.properties.Image.files[0].file.url
           pages.push(page)
         })
 
@@ -42,18 +43,29 @@ const Blog = () => {
       {
         blogPosts.map(function(post){
           return (
-            <div class="content-box blog-post">
-              <div class="post-title">
-                {post.title}
+            <div class="blog-post">
+              <div class="blog-post-image">
+                <div>
+                  <img src={post.photo} width="300px" height="225px" />
+                </div>
               </div>
-              <div>
-                {post.caption}
-              </div>
-              <div>
-                {post.date}
-              </div>
-              <div>
-                {post.location}
+              <div class="blog-post-details">
+                <div>
+                  <div class="blog-post-title">
+                    {post.title}
+                  </div>
+                  <div class="blog-post-caption">
+                    {post.caption}
+                  </div>
+                </div>
+                <div>
+                  <div class="blog-post-date">
+                    {post.date}
+                  </div>
+                  <div class="blog-post-location">
+                    {post.location}
+                  </div>
+                </div>
               </div>
             </div>
           )
