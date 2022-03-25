fdec7350921c09b09811618512330159fa7941e5
Lauren <lauren@laurens-mbp.home>
2022-03-25 20:48:30 +0000

    Add Notion API client

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,34 +1,25 @@
 import * as React from "react"
+import {getAllBlogPosts} from "../api/client"
 
 
 const Blog = () => {
   const [blogPosts, setBlogPosts] = React.useState([])
 
   React.useEffect(() => {
-    fetch(`https://blooming-peak-64334.herokuapp.com/https://api.notion.com/v1/databases/8e5e6b0aa3144af3bb45fbedf719979a/query`, {
-      method: "POST",
-      mode: 'cors',
-      headers: {
-        "Notion-Version": "2022-02-22",
-        "Authorization": "Bearer secret_bMVein24eDYAwBss2Np3XoZEA0XlOc1rFZM8dqiKn7F",
-      }
-  })
-      .then(response => response.json())
-      .then(responseData => {
-        let pages = []
-        responseData.results.map(function(result){
-          console.log(result)
-          let page = {}
-          page.title = result.properties.Title.title[0].plain_text
-          page.date = result.properties.Date.date.start
-          page.location = result.properties.Location.rich_text[0].plain_text
-          page.caption = result.properties.Caption.rich_text[0].plain_text
-          page.photo = result.properties.Image.files[0].file.url
-          pages.push(page)
-        })
-
-        setBlogPosts(pages)
+    getAllBlogPosts().then(responseData => {
+      let pages = []
+      responseData.results.map(function(result){
+        let page = {}
+        page.title = result.properties.Title.title[0].plain_text
+        page.date = result.properties.Date.date.start
+        page.location = result.properties.Location.rich_text[0].plain_text
+        page.caption = result.properties.Caption.rich_text[0].plain_text
+        page.photo = result.properties.Image.files[0].file.url
+        pages.push(page)
       })
+
+      setBlogPosts(pages)
+    })
   }, [])
 
   return (
