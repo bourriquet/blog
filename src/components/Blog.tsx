f959becefbb785fc5a083cd15f2046dcbeebe138
Lauren <laurenanders92@gmail.com>
2022-03-29 23:15:21 +0100

    Use local store of blog posts

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,34 +1,15 @@
 import * as React from "react"
-import {getAllBlogPosts} from "../api/client"
+import {blogPosts} from "../data/blogPosts"
 
 import BlogPost from "./blog/BlogPost"
 
 
 const Blog = () => {
-  const [blogPosts, setBlogPosts] = React.useState([])
-
-  React.useEffect(() => {
-    getAllBlogPosts().then(responseData => {
-      let pages = []
-      responseData.results.map(function(result){
-        let page = {}
-        page.id = result.id
-        page.title = result.properties.Title.title[0].plain_text
-        page.date = result.properties.Date.date.start
-        page.location = result.properties.Location.rich_text[0].plain_text
-        page.caption = result.properties.Caption.rich_text[0].plain_text
-        page.photo = result.properties.Image.files[0].file.url
-        pages.push(page)
-      })
-
-      setBlogPosts(pages)
-    })
-  }, [])
-
   return (
     <div className="content">
       {
-        blogPosts.map(function(post, index){
+        blogPosts().map(function(post, index){
+          console.log(post)
           return (
             <BlogPost
               key={index}
---
+++ src/data/blogPosts.ts
@@ -0,0 +1,11 @@
+const blogPosts = () => [{
+    id: "a18b39a6ed634866a38f58bc7a9e8885",
+    date: "2022-03-29",
+    photo: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4edeb25b-26ef-4a2b-80d6-85ff1c44aa19/artboard-4-copy-22403x.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220329%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220329T220221Z&X-Amz-Expires=3600&X-Amz-Signature=43d0a0324f3232a4ae47f4f0503cbe3d1903b1e7af0e7432806a9a562b989e07&X-Amz-SignedHeaders=host&x-id=GetObject",
+    title: "Kicking off my Rebel Badge journey",
+    caption: "Collecting",
+    tags: ["rebel_badge_club"],
+    location: "London, UK"
+}]
+
+export {blogPosts};
