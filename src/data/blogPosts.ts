cc9fc6f99696ac4f4b17fd5c9aa120f3c943b716
Lauren <laurenanders92@gmail.com>
2022-03-31 21:26:42 +0100

    Change image for blog post

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -1,7 +1,7 @@
 const blogPosts = () => [{
-    id: "a18b39a6ed634866a38f58bc7a9e8885",
+    id: "867f4034-b8da-4bdc-a4e9-902c13b6f83e",
     date: new Date(2022, 3, 30),
-    photo: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4edeb25b-26ef-4a2b-80d6-85ff1c44aa19/artboard-4-copy-22403x.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220329%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220329T220221Z&X-Amz-Expires=3600&X-Amz-Signature=43d0a0324f3232a4ae47f4f0503cbe3d1903b1e7af0e7432806a9a562b989e07&X-Amz-SignedHeaders=host&x-id=GetObject",
+    photo: "https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/867f4034-b8da-4bdc-a4e9-902c13b6f83e_cover",
     title: "Kicking off my Rebel Badge journey",
     caption: "Collecting",
     tags: ["rebel_badge_club"],
