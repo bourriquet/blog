4ff2f9df8084f2cce525708c0489ad6ef9e79fdc
Lauren <laurenanders92@gmail.com>
2022-04-06 07:48:00 +0100

    Add empty blog post holding page

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -5,15 +5,19 @@ import Sidebar from "../components/sidebar/Sidebar"
 import "../index.css"
 
 const PostPage = (props) => {
+  const [pageId, setPageId] = React.useState("None")
+
+  React.useEffect(() => {
+    setPageId(props.id)
+  })
+
   return (
     <div className="columns">
      <div className="sidebar">
        <Sidebar />
      </div>
      <div className="content-container">
-       <Post
-        id={props.id}
-       />
+       <Post id={pageId}/>
      </div>
   </div>
   )
