c5d383dc350044633df1cc2b7ecfd71180b0db11
Lauren <laurenanders92@gmail.com>
2022-04-06 08:35:01 +0100

    Fix routing

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,8 +3,6 @@ const path = require("path");
 exports.onCreatePage = async ({page, actions}) =>{
     const {createPage, createRedirect} = actions
 
-    createRedirect({ fromPath: '/*', toPath: '/'})
-
     if(page.path.match(/^\/post/)){
         createPage({
             path: "/post",
@@ -12,4 +10,6 @@ exports.onCreatePage = async ({page, actions}) =>{
             component: path.resolve("src/pages/post.tsx")
         })
     }
+
+    createRedirect({ fromPath: '/*', toPath: '/'})
 }
--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -2,8 +2,8 @@ const getBlogPost = (id) => blogPosts().filter(post => post.id == id)[0]
 
 const blogPosts = () => [{
     id: "867f4034-b8da-4bdc-a4e9-902c13b6f83e",
-    date: new Date(2022, 3, 5),
-    date_published: new Date(2022, 3, 5),
+    date: new Date(2022, 3, 4),
+    date_published: new Date(2022, 3, 6),
     photo: "https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/867f4034-b8da-4bdc-a4e9-902c13b6f83e_cover",
     title: "Kicking off my Rebel Badge journey",
     caption: "53 badges to collect",
