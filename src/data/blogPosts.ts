8d0daf894c43edc22c033e915fb58de514f3f555
Lauren <laurenanders92@gmail.com>
2022-04-06 09:10:35 +0100

    Tweak blog post

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -6,7 +6,7 @@ const blogPosts = () => [{
     date_published: new Date(2022, 3, 6),
     photo: "https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/867f4034-b8da-4bdc-a4e9-902c13b6f83e_cover",
     title: "Kicking off my Rebel Badge journey",
-    caption: "53 badges to collect",
+    caption: "How many badges can I tick off?",
     tags: ["rebel_badge_club"],
     location: "London, UK"
 }]
