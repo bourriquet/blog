1c085804233460fea3ea4660d86b604985dce6bf
Lauren <laurenanders92@gmail.com>
2022-04-07 17:35:30 +0100

    Add Google Analytics

--- src/pages/post/867f4034-b8da-4bdc-a4e9-902c13b6f83e.tsx
+++ src/pages/post/867f4034-b8da-4bdc-a4e9-902c13b6f83e.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import ReactGA from "react-ga"
 import PostContent from "../../components/blog/posts/867f4034-b8da-4bdc-a4e9-902c13b6f83e"
 import PostTemplate from "../../components/templates/Post"
 
@@ -7,6 +8,8 @@ import {getBlogPost} from "../../data/blogPosts"
 const postData = getBlogPost("867f4034-b8da-4bdc-a4e9-902c13b6f83e")
 
 const Post = () => {
+  React.useEffect(() => ReactGA.pageview(window.location.pathname))
+  
   return (
     <PostTemplate
       caption={postData.caption}
