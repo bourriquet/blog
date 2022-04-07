cec74ef045857852801346a9daafb7516de671ad
Lauren <laurenanders92@gmail.com>
2022-04-07 23:15:05 +0100

    Add favicon

--- src/components/templates/Page.tsx
+++ src/components/templates/Page.tsx
@@ -1,18 +1,26 @@
 import * as React from "react"
+import { Helmet } from "react-helmet"
 import Sidebar from "../sidebar/Sidebar"
+import favicon from "../../images/favicon.png"
 
 import "../../index.css"
 
 const PageTemplate = (props) => {
   return (
-    <div className="columns">
-     <div className="sidebar">
-       <Sidebar />
-     </div>
-     <div className="content-container">
-       {props.page}
-     </div>
-    </div>
+    <>
+      <Helmet>
+        <title>Lauren Anders</title>
+        <link rel="icon" type="image/png" href={favicon} />
+      </Helmet>
+      <div className="columns">
+       <div className="sidebar">
+         <Sidebar />
+       </div>
+       <div className="content-container">
+         {props.page}
+       </div>
+      </div>
+    </>
   )
 }
 
