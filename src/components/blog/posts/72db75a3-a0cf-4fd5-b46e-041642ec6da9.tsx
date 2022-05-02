48b0b61f4443b42c32a3b18e3db2442d03345104
Lauren <laurenanders92@gmail.com>
2022-05-02 21:51:59 +0100

    Add blog post

---
+++ src/components/blog/posts/72db75a3-a0cf-4fd5-b46e-041642ec6da9.tsx
@@ -0,0 +1,188 @@
+import * as React from "react"
+import PostImageSingle from "../PostImageSingle"
+
+const PostContent = (props) => {
+  return (
+    <div className="standard-content">
+      <p>
+        Two sentences I never thought I’d say: <em>“I’m going to cycle 1,000
+        miles across the country”</em> and <em>“I’m going to spend 15
+        consecutive nights in a tent”</em>. I’m very much a fair-weather cyclist
+        and I've only ever camped at music festivals, but here I am, about to
+        set off on a cycle trip from Land’s End to John o’Groats, camping along
+        the way!
+      </p>
+
+      <p>
+        Cycling either LEJOG (Land’s End to John o’Groats) or JOGLE (John
+        o’Groats to Land’s End) has never been anywhere close to my bucket list.
+        However, with the uncertainty around coronavirus quarantine rules and
+        entry requirements, my partner Jonathan and I had resigned to not
+        travelling abroad for a holiday this year. While looking for things we
+        could do in the UK, the idea of a LEJOG or JOGLE cycle came to mind and
+        the more I researched it, the more I wanted to give it a go myself. If
+        not now, then when?
+      </p>
+
+      <p>
+        A lot of preparation has gone into this trip, much more than I had
+        initially anticipated. We had equipment to buy, both cycling and
+        camping, as well as campsites to find, and, most importantly, a mammoth
+        route to plan. Reading LEJOG/JOGLE blogs has been invaluable to me, so
+        I’m hoping that this post about some of the things we've had to prepare
+        before setting off will be of use to future end-to-enders.
+      </p>
+
+      <h3>Going It Alone</h3>
+      <p>
+        This was one of the first decisions we made, and it was quite an easy
+        one for us . There is no shortage of companies offering LEJOG/JOGLE
+        packages - both "supported" (you cycle as a group, stay in B&Bs
+        together, have a support van that carries your things between stops) and
+        "independent" (the company books your accommodation for you and plans
+        your route, but you cycle by yourself).
+      </p>
+
+      <p>
+        This was going to be our summer holiday, so we wanted to take our time,
+        stop whenever we wanted to and see some sights. Cycling as part of an
+        organised group would have perhaps taken some of that freedom away from
+        us. There was also something about planning the entire thing ourselves
+        and carrying everything with us that added to the fun!
+      </p>
+
+      <h3>The Route</h3>
+      <p>
+        I wasn't too keen on cycling on A-roads the whole way, so the fastest route was off
+        the table. The <a target="_blank" href="https://www.sustrans.org.uk/national-cycle-network/lands-end-to-john-ogroats-lejog">Sustrans route</a> was
+        a good candidate as it is entirely on traffic-free paths and quiet roads
+        - perfect for me! However, I was planning on taking my road bike, which
+        wouldn't have coped well on some of the off-road paths, and I wasn't up
+        for buying a new bike just for this trip.
+      </p>
+
+      <p>
+        The <a target="_blank" href="https://www.cicerone.co.uk/why-cicerone-lejog-cycling-route-optimal-choice">Cicerone route</a> claims
+        to be the "optimal" route - quiet roads where possible, busier roads
+        where necessary. The route has been tried and tested on multiple bike
+        types and adapted over the last twelve years. The recommended time for
+        this route is two weeks, which was perfect for us given our other summer
+        plans and needing to get back to work. The only downside to this route
+        is that the quieter traffic-free roads tend to come at a cost... hills.
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Our bible for the summer"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/72db75a3-a0cf-4fd5-b46e-041642ec6da9_cover"
+        />
+      </p>
+
+      <p>
+        We plan to follow the Cicerone route as closely as we can, though will
+        be adapting some of the recommended overnight stops along the way. We
+        want to spend more time in Cornwall and Scotland - Cornwall to warm up
+        the legs; Scotland to be tourists as neither of us have visited for a
+        few years. I was born in the North West, so I'm quite happy to speed
+        through here to make up lost time... and it will also be the flattest
+        part of the route! We also have no rest days planned, aiming for ~80
+        miles/day.
+      </p>
+
+      <h3>LEJOG or JOGLE?</h3>
+      <p>
+        LEJOG is generally the preferred route among end-to-enders. As you’re
+        cycling in a mostly northerly direction, the sun is behind you and out
+        of your eyes, and the rumour is that the prevailing south-westerly wind
+        helps you along the way. The cycling is also toughest in
+        Cornwall, with steep inclines and declines... something I definitely
+        wanted to tackle at the start of the trip before the exhaustion had set
+        in! Though there is the argument that by doing JOGLE, the Cornish hills
+        should be easier as your cycling fitness will have improved having
+        already climbed plenty of hills before reaching Cornwall.
+      </p>
+
+      <p>
+        Living in London, it would be fairly straight-forward to get to and from
+        either end. Between London and Land’s End, the journey is
+        quite simple - a 5 hour train from Paddington to Penzance (there is also
+        the option of a sleeper train), then a 15 mile cycle to Land’s End.
+        Between London and John o’Groats, it’s a bit more complicated. Thurso
+        and Wick are both ~20 miles from John o’Groats, from where you can catch
+        one of a handful of trains per day to Inverness. Between Inverness and
+        London, it’s a 9+ hour train with changes or you can catch a direct 11
+        hour sleeper train, the Caledonian Sleeper. Flying to/from Inverness was
+        also an option, but we would then have to sort out
+        transporting/renting/buying a bike box, adding extra cost and hassle.
+        Travelling by train felt like the most sensible option.
+      </p>
+
+      <p>
+        It’s free to take a bike onto a train in the UK, though reservations are
+        usually compulsory. These get snapped up pretty quickly for
+        long-distance journeys, especially on the Caledonian Sleeper. We knew we
+        wanted to go in July or August (better weather, more sunlight) and had a
+        preference for LEJOG, though it was the availability of bike spots on
+        the Caledonian Sleeper that eventually determined our dates and
+        direction. There were two bike spots available on a Tuesday night from
+        Inverness to London in August - LEJOG it was!
+      </p>
+
+      <h3>Sleeping</h3>
+      <p>
+        Of course, we weren't the only ones looking to stay in the UK for a
+        summer holiday given the year of the staycation. Cycle-friendly
+        hotel/B&B availability was looking sparse, and much more expensive than
+        we were prepared to pay for this trip. We figured camping would give us
+        more flexibility and save us some money, and would also add to the
+        adventure. The real adventure would have been to wild camp the entire
+        way, but we wanted to keep the camping equipment we needed to carry to a
+        minimum. I also didn't want to be washing in lakes for two weeks!
+      </p>
+
+      <p>
+        Finding campsites with availability in August was difficult, and many
+        had 2-night minimum bookings. After a lot of enquiring and shuffling
+        around of our planned stops, we eventually found campsites with
+        availability for most nights relatively close to the
+        Cicerone route. The exception was the Bristol area, for which we booked
+        an Airbnb. This feels like cheating a little bit, but I don't think
+        we'll be complaining when we're in a warm bed!
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="LEJOG via campsite (Google Maps)"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/72db75a3-a0cf-4fd5-b46e-041642ec6da9_1"
+          portrait
+        />
+      </p>
+
+      <h3>Sight-seeing</h3>
+      <p>
+        It's a holiday! We want to see things. A few weeks ago, I was pinged by
+        the NHS app to self-isolate as I had been in contact with someone who
+        had tested positive for Covid-19. I luckily didn't catch Covid-19
+        myself, but I used the time to pore through the numerous travel books
+        I'd already checked out from the library and put together a list of
+        things we could see along the way. Easier said than done, as we will be
+        lumbered with two bikes and all of our things, but we might be able to
+        tick some of them off.
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Points of interest along the way (Google Maps). I feel like I may have been a bit harsh on the Midlands/North West..."
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/72db75a3-a0cf-4fd5-b46e-041642ec6da9_2"
+          portrait
+        />
+      </p>
+
+      <h3>The Noble Steeds</h3>
+      
+
+    </div>
+  )
+}
+
+export default PostContent;
