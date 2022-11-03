886a06460cc89c32db7125ede1df69c7b3e0e2f5
Lauren <laurenanders92@gmail.com>
2022-11-03 08:41:35 +0000

    Add blog post

---
+++ src/components/blog/posts/48427dad-dc2c-49e9-8c9e-b4a0de45785a.tsx
@@ -0,0 +1,379 @@
+import * as React from "react"
+import PostImageSingle from "../PostImageSingle"
+import { Link } from "gatsby"
+
+const PostContent = (props) => {
+  return (
+    <div className="standard-content">
+      <p>
+        <em>While cycling from Lands End to John o'Groats was a trip of a
+        lifetime, I also fundraised for <a href="https://www.mind.org.uk/" target="_blank">Mind UK</a> along
+        the way. My fundraising page has closed now, but if you would like to
+        donate to this fantastic charity, donations are accepted via their
+        website.</em>
+      </p>
+
+      <p>
+        Months of <Link to="/post/72db75a3-a0cf-4fd5-b46e-041642ec6da9" target="_blank">preparation for LEJOG</Link> and the big day had finally arrived! We
+        were off to Lands End to begin our 1,000 mile journey across the length
+        of the UK.
+      </p>
+
+      <h3>Streatham to Paddington</h3>
+      <p>
+        We'd decided to get the train to Penzance then cycle to Lands End, where
+        we’d be spending the night before the Grand Départ the next morning. Our
+        first test was the cycle to Paddington station with our bikes loaded up
+        with all of our things for the next 16 days. Of course, it was pouring
+        down with rain for the first cycle of the trip, thankfully not a sign of
+        things to come. First hurdle for me… actually getting on my bike. The
+        weight and width of the loaded panniers made things a little unstable,
+        and my bike nearly ended up flat on the floor while I was trying to get
+        on it. Second attempt and I was safely on.
+      </p>
+
+      <p>
+        It’s a fairly straight-forward cycle to Paddington for us, though this
+        was both of our first cycles with fully-laden bikes. We’re both quite
+        used to cycling with the London traffic, Jonathan more so and much more
+        confident with it than I am. A few km in and I was thinking that this
+        actually wasn’t all that bad (my cycles often turn out like this…), and
+        the nerves slowly started to turn into excitement for the days ahead.
+      </p>
+
+      <p>
+        One wrong turn and just under an hour later, we were safely at
+        Paddington, with plenty of time to stock up on snacks (the carb-loading
+        starts here!) before the 5 hour journey to Penzance.
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="We survived the very first cycle!"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_1"
+          portrait
+        />
+      </p>
+
+      <p align="center">
+        <iframe height='405' width='590' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/activities/5762488219/embed/0ec6665716b28ffc51eb8329087a3495b62993c8' target="_blank"></iframe>
+      </p>
+
+      <h3>End of the line</h3>
+      <p>
+        Boarding started late and the train was fully booked, so it felt a
+        little hectic trying to unload our bikes and get them racked in time.
+        Our seats were at the other end of the train to our allocated bike spots
+        so I held down the fort on the platform minding our bags while Jonathan
+        ran back and forth with both of the bikes.
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Almost time to go..."
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_2"
+          portrait
+        />
+      </p>
+
+      <p>
+        Just over three hours later, we were crossing the River Tamar into
+        Cornwall, over Brunel's famous Royal Albert Bridge. Strange to think
+        that in less than 48 hours, we'd be crossing the river in the other
+        direction, but by ferry rather than train.
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="The not-as-famous Tamar Bridge"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_3"
+          portrait
+        />
+      </p>
+
+      <p>
+        Once we were in Cornwall, the train started to thin out and we were
+        soon in Penzance, the end of the line!
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="All you need for 16 days of camping and cycling"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_4"
+          portrait
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_5"
+          portrait
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Loaded up and ready to go again"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_6"
+          portrait
+        />
+      </p>
+
+      <h3>Penzance</h3>
+      <p>
+        This was both of our first times in Penzance, but we weren’t sticking
+        around for too long as we were keen to get to Lands End to set up camp.
+        Just outside the station, a sign welcomes you to Penzance in the Cornish
+        language.
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Pensans a'gas dynergh"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_7"
+          portrait
+        />
+      </p>
+
+      <p>
+        We wanted to try out <a href="http://www.willywallers.co.uk/" target="_blank">Willy Wallers</a> ice
+        cream, just a short walk from the train station. We saw a few groups of
+        cyclists cycle past with Prostate Cancer UK jerseys on heading towards
+        Lands End for the end of their JOGLE. Now very close to the point of no
+        return and faced with having to cycle on unfamiliar roads, my nerves had
+        well and truly returned, but seeing people about to finish their
+        End-to-End journey and having a great time doing so gave me a little bit
+        of confidence. My sorbet from Willy Wallers was also a very welcome
+        distraction!
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Sorbet and palm trees"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_8"
+          portrait
+        />
+      </p>
+
+      <h3>Penzance to Lands End</h3>
+      <p>
+        The Cicerone recommended route between Penzance and Lands End is the
+        A30, which we would be taking the next day on our return to Penzance,
+        but I’d planned an alternative, more scenic coastal route to take us to
+        our campsite while we had the time. All going well, I was hoping we
+        could cycle down to a beach and spend a couple of hours there.
+      </p>
+
+      <p>
+        However, I’d made one catastrophic mistake… I hadn’t checked the
+        elevation profile. As soon as we’d left Penzance, it was straight into a
+        14% incline. No easing ourselves into it or getting used to the added
+        weight on our bikes. It was a bit of a shock to the system, and I ended
+        up walking most of this first incline, which sparked a few laughs from a
+        passing postman - they must see this all of the time!
+      </p>
+
+      <p>
+        The downhills weren’t much better. I wasn’t yet used to the speed the
+        added weight gives you on a decline and I found myself hanging on for
+        dear life. We were also on a 60mph country lane, a far cry from the
+        gridlock London traffic I’m used to, so we had no choice but to
+        persevere with the hills. At least the scenery was good - green Cornish
+        countryside either side of us and the sea fast-approaching in the
+        distance.
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Getting closer..."
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_9"
+        />
+      </p>
+
+      <p>
+        We gave up on the beach trip pretty easily. We were already pretty high
+        above sea level and there was no way we were subjecting ourselves to
+        more hills than we needed to! We’d taken more time to get to Lands End
+        than we thought we would, but we turned one last corner and our campsite
+        was finally in sight.
+      </p>
+
+      <p align="center">
+        <iframe height='405' width='590' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/activities/5764509370/embed/e5f61d2e9c894054acf49f10d8e1cae7f1f75439' target="_blank"></iframe>
+      </p>
+
+      <h3>Lands End</h3>
+      <p>
+        We were staying at the recently-opened <a href="https://www.landsendcamp.co.uk/" target="_blank">Lands End Camping & Glamping</a>,
+        less than a mile away from the famous Lands End landmark. We very
+        clearly looked like we were about to start LEJOG as the owners were
+        laughing at our decision to bring an electric hook-up, and bet that we
+        would end up ditching it within a few days. They even offered to buy it
+        from us for £15, but we were determined to get some use out of it.
+      </p>
+
+      <p>
+        Tent pitched up, electric hook-up untangled and having worked up some
+        appetite on the surprise hills on our cycle to Lands End, we walked the
+        short distance into Sennen to grab some food. We walked past the
+        first/last inn in England - can't wait to see the first/last inn in
+        Scotland at the other end!
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Our new home"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_10"
+          portrait
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="First & Last Inn in England"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_11"
+        />
+      </p>
+
+      <p>
+        There weren’t too many options for food in Sennen, so, being by the
+        seaside, it had to be a chippy. I think everyone else in Lands End had
+        the same idea, as we had quite a long wait outside for our food. The
+        perfect opportunity to pick up some local Tribute ale.
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Tribute and chips"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_12"
+          portrait
+        />
+      </p>
+
+      <p>
+        We walked down to the Lands End landmark to check things out. I'd only
+        read bad things about Lands End, that it’s purely a tourist trap (true)
+        and that there isn't much to see or do (also true). We’d really lucked
+        out with the beautiful sunset, which only added to the stunning views
+        from the most south-westernly tip of mainland England.
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_13"
+          portrait
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_14"
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_15"
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_16"
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_17"
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_18"
+          portrait
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="Morph!"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_19"
+          portrait
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_20"
+        />
+      </p>
+
+      <p>
+        It was the perfect time for photos with the famous signpost, as most of
+        the crowds had died down and the peddlers charging £15 for a photo had
+        already left for the day. We also saw someone who had just finished
+        their JOGLE cycle!
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_21"
+          portrait
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_22"
+          portrait
+        />
+      </p>
+
+      <p>
+        We walked along the coastline to stretch our legs, hoping to make it to
+        Sennen Cove or at least catch a glimpse of the RMS Mulheim shipwreck.
+        Unfortunately we didn't get as far as the shipwreck as it was getting
+        dark quite quickly, but the walk along the coastline was a lovely end
+        to a long day regardless.
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_24"
+          portrait
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_23"
+        />
+      </p>
+
+      <p>
+        Back to the campsite for a timed 5 minute token shower and an early
+        night ahead of the big day tomorrow. I hadn’t seen such a clear sky in a
+        very long time!
+      </p>
+
+      <p>
+        <PostImageSingle
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_25"
+        />
+      </p>
+
+      <p>
+        <PostImageSingle
+          caption="So many stars!"
+          image="https://lauren-anders-blog.s3.eu-west-2.amazonaws.com/48427dad-dc2c-49e9-8c9e-b4a0de45785a_26"
+          portrait
+        />
+      </p>
+
+    </div>
+  )
+}
+
+export default PostContent;
